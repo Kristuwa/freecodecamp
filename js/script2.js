50d6233712baed84c87122bb9dc516653d9d3cd1
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-08-23 19:45:50 +0300

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -18,50 +18,74 @@
 // );
 
 // Налаштування
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
 
-function lookUpProfile(name, prop) {
-  // Змініть код лише під цим рядком
-  let message = "";
-  for (const contact of contacts) {
-    if (contact.firstName === name && contact[prop]) {
-      return contact[prop];
-    } else if (contact.firstName === name && !contact[prop]) {
-      return (message = "No such property");
-    } else {
-      message = "No such contact";
-    }
-  }
-  return message;
-  // Змініть код лише над цим рядком
-}
-console.log(lookUpProfile("Kristian", "lastName"));
-console.log(lookUpProfile("Sherlock", "likes"));
-console.log(lookUpProfile("Harry", "likes"));
-console.log(lookUpProfile("Akira", "address"));
-// lookUpProfile("Akira", "likes");
+// function lookUpProfile(name, prop) {
+//   // Змініть код лише під цим рядком
+//   let message = "";
+//   for (const contact of contacts) {
+//     if (contact.firstName === name && contact[prop]) {
+//       return contact[prop];
+//     } else if (contact.firstName === name && !contact[prop]) {
+//       return (message = "No such property");
+//     } else {
+//       message = "No such contact";
+//     }
+//   }
+//   return message;
+//   // Змініть код лише над цим рядком
+// }
+// console.log(lookUpProfile("Kristian", "lastName"));
+// console.log(lookUpProfile("Sherlock", "likes"));
+// console.log(lookUpProfile("Harry", "likes"));
+// console.log(lookUpProfile("Akira", "address"));
+// // lookUpProfile("Akira", "likes");
+
+// function countup(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const countArray = countup(n - 1);
+//     countArray.push(n);
+//     return countArray;
+//   }
+// }
+// console.log(countup(5));
+// Math.floor(Math.random() * (max - min + 1)) + min;
+
+// function rangeOfNumbers(startNum, endNum) {
+//   if (startNum > endNum) {
+//     return [];
+//   } else {
+//     const countArray = rangeOfNumbers(startNum, endNum - 1);
+//     countArray.push(endNum);
+//     return countArray;
+//   }
+// }
+
+// console.log(rangeOfNumbers(1, 5));
